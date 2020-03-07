20b62956c5fa2852b38df89101bcf7fa4fe4a4d2
seanyen <seanyen@microsoft.com>
2020-03-07 11:04:24 -0800

    use setup.cfg.

--- setup.py
+++ setup.py
@@ -1,28 +1,3 @@
 import setuptools
-from vinca import __version__
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="vinca",
-    version=__version__,
-    author="Sean Yen",
-    author_email="zishenyan@gmail.com",
-    description="Conda recipe generator for ROS packages",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/seanyen/vinca",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.7',
-    entry_points={
-        'console_scripts': [
-            'vinca = vinca.main:main',
-        ]
-    }
-)
+setuptools.setup()
