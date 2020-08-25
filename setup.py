6d485d7a865303b5a6b4d8ae1370d6784e0e98d7
Carlos Herrero <carlosherrerocontact@gmail.com>
2020-08-25 14:53:43 +0200

    Solve Poco

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
-import setuptools
+from setuptools import find_packages, setup
 
-setuptools.setup()
+args = {
+    'include_package_data': True,
+    'packages': find_packages(),
+    'package_data': {
+        'vinca': [
+            'templates/*'
+        ]
+    },
+}
+
+setup(**args)
