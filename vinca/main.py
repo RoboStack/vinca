88ad9ce047982bc340acbeb81d2b848d2e6a5b99
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-09-01 13:18:41 +0200

    add generation of azure and gitlab pipelines

--- vinca/main.py
+++ vinca/main.py
@@ -139,10 +139,10 @@ def generate_output(pkg_shortname, vinca_conf, distro):
     if pkg.get_build_type() in ['cmake', 'catkin']:
         # TODO find a way to get the conda "comments" with ruamel
         # output['script'] = ['bld_catkin.bat  # [win]', 'build_catkin.sh  # [unix]']
-        if sys.platform.startswith('win'):
-            output['build']['script'] = 'bld_catkin.bat'
-        else:
-            output['build']['script'] = 'build_catkin.sh'
+        output['build']['script'] = {
+            'sel(win)': 'bld_catkin.bat',
+            'sel(unix)': 'build_catkin.sh'
+        }
 
     elif pkg.get_build_type() in ['ament_cmake']:
         output['build']['script'] = 'bld_ament_cmake.bat'
@@ -371,7 +371,10 @@ def generate_source(distro, vinca_conf):
                 plat = 'win'
             patches.extend(pd[plat])
             if len(patches):
-                entry['patches'] = patches
+                print(patches)
+                common_prefix = os.path.commonprefix((os.getcwd(), patches[0]))
+                print(common_prefix)
+                entry['patches'] = [os.path.relpath(p, common_prefix) for p in patches]
 
         source[pkg_name] = entry
 
