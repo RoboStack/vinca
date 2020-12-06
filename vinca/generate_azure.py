64a271a5b93be475f31f8a6ee7920e200a40ff7c
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-12-06 23:53:54 +0100

    fix win pipelines

--- vinca/generate_azure.py
+++ vinca/generate_azure.py
@@ -43,7 +43,8 @@ export FEEDSTOCK_NAME=$(basename ${BUILD_REPOSITORY_NAME})
 
 azure_win_preconfig_script = literal_unicode("""\
 set "CI=azure"
-call activate base
+call %CONDA%\\condabin\\conda_hook.bat
+call %CONDA%\\condabin\\conda.bat activate base
 
 :: 2 cores available on Appveyor workers: https://www.appveyor.com/docs/build-environment/#build-vm-configurations
 :: CPU_COUNT is passed through conda build: https://github.com/conda/conda-build/pull/1149
@@ -102,7 +103,8 @@ conda list --show-channel-urls
 
 azure_win_script = literal_unicode("""\
 setlocal EnableExtensions EnableDelayedExpansion
-call activate base
+call %CONDA%\\condabin\\conda_hook.bat
+call %CONDA%\\condabin\\conda.bat activate base
 
 set "FEEDSTOCK_ROOT=%cd%"
 
@@ -496,7 +498,7 @@ def main():
                             "displayName": "Add conda to PATH"
                         },
                         {
-                            "script": 'conda install -c conda-forge --yes --quiet conda-build pip mamba ruamel.yaml',
+                            "script": 'conda install -c conda-forge --yes --quiet conda-build pip mamba ruamel.yaml anaconda-client',
                             "displayName": "Install conda-build, boa and activate environment"
                         },
                         {
