f02724f4875461653c8f1db5b8cb1bf87d9a0e7c
Tobias Fischer <info@tobiasfischer.info>
2021-06-10 09:40:16 +1000

    Try pkg-config again

--- vinca/main.py
+++ vinca/main.py
@@ -321,11 +321,16 @@ def generate_output(pkg_shortname, vinca_conf, distro, version):
         output["requirements"]["build"] += [
             {"sel(build_platform != target_platform)": "pybind11"}
         ]
-    # pkg-config must be in build, not host
+    # pkg-config must be in build, not host for cross-compile
     if "pkg-config" in output["requirements"]["host"]:
-        output["requirements"]["build"].append("pkg-config")
+        output["requirements"]["build"] += [
+            {"sel(build_platform != target_platform)": "pkg-config"}
+        ]
         while "pkg-config" in output["requirements"]["host"]:
             output["requirements"]["host"].remove("pkg-config")
+        output["requirements"]["host"] += [
+            {"sel(build_platform == target_platform)": "pkg-config"}
+        ]
 
     # fixup problems with udev (which is mapped to libusb):
     if "libusb" in output["requirements"]["host"]:
