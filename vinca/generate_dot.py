abd07b58ed6c3d2308255e2f39308e45cb49109f
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-07-23 17:54:58 +0200

    simplify dot generator

--- vinca/generate_dot.py
+++ vinca/generate_dot.py
@@ -7,141 +7,14 @@ import textwrap
 import argparse
 from distutils.dir_util import copy_tree
 import yaml
-
-
-class folded_unicode(str):
-    pass
-
-
-class literal_unicode(str):
-    pass
-
-
-def folded_unicode_representer(dumper, data):
-    return dumper.represent_scalar("tag:yaml.org,2002:str", data, style=">")
-
-
-def literal_unicode_representer(dumper, data):
-    return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
-
-
-yaml.add_representer(folded_unicode, folded_unicode_representer)
-yaml.add_representer(literal_unicode, literal_unicode_representer)
-
-
-azure_linux_script = literal_unicode("""\
-export CI=azure
-export GIT_BRANCH=$BUILD_SOURCEBRANCHNAME
-export FEEDSTOCK_NAME=$(basename ${BUILD_REPOSITORY_NAME})
-.scripts/run_docker_build.sh""")
-
-azure_osx_script = literal_unicode(r"""\
-export CI=azure
-export GIT_BRANCH=$BUILD_SOURCEBRANCHNAME
-export FEEDSTOCK_NAME=$(basename ${BUILD_REPOSITORY_NAME})
-.scripts/build_osx.sh""")
-
-azure_osx_arm64_script = literal_unicode(r"""\
-export CI=azure
-export GIT_BRANCH=$BUILD_SOURCEBRANCHNAME
-export FEEDSTOCK_NAME=$(basename ${BUILD_REPOSITORY_NAME})
-.scripts/build_osx_arm64.sh""")
-
-azure_win_preconfig_script = literal_unicode("""\
-set "CI=azure"
-call %CONDA%\\condabin\\conda_hook.bat
-call %CONDA%\\condabin\\conda.bat activate base
-
-:: 2 cores available on Appveyor workers: https://www.appveyor.com/docs/build-environment/#build-vm-configurations
-:: CPU_COUNT is passed through conda build: https://github.com/conda/conda-build/pull/1149
-set CPU_COUNT=2
-
-set PYTHONUNBUFFERED=1
-
-conda config --set show_channel_urls true
-conda config --set auto_update_conda false
-conda config --set add_pip_as_python_dependency false
-
-call setup_x64
-
-:: Set the conda-build working directory to a smaller path
-if "%CONDA_BLD_PATH%" == "" (
-    set "CONDA_BLD_PATH=C:\\bld\\"
-)
-
-:: Remove some directories from PATH
-set "PATH=%PATH:C:\\ProgramData\\Chocolatey\\bin;=%"
-set "PATH=%PATH:C:\\Program Files (x86)\\sbt\\bin;=%"
-set "PATH=%PATH:C:\\Rust\\.cargo\\bin;=%"
-set "PATH=%PATH:C:\\Program Files\\Git\\usr\\bin;=%"
-set "PATH=%PATH:C:\\Program Files\\Git\\cmd;=%"
-set "PATH=%PATH:C:\\Program Files\\Git\\mingw64\\bin;=%"
-set "PATH=%PATH:C:\\Program Files (x86)\\Subversion\\bin;=%"
-set "PATH=%PATH:C:\\Program Files\\CMake\\bin;=%"
-set "PATH=%PATH:C:\\Program Files\\OpenSSL\\bin;=%"
-set "PATH=%PATH:C:\\Strawberry\\c\\bin;=%"
-set "PATH=%PATH:C:\\Strawberry\\perl\\bin;=%"
-set "PATH=%PATH:C:\\Strawberry\\perl\\site\\bin;=%"
-set "PATH=%PATH:c:\\tools\\php;=%"
-
-:: On azure, there are libcrypto*.dll & libssl*.dll under
-:: C:\\Windows\\System32, which should not be there (no vendor dlls in windows folder).
-:: They would be found before the openssl libs of the conda environment, so we delete them.
-if defined CI (
-    DEL C:\\Windows\\System32\\libcrypto-1_1-x64.dll || (Echo Ignoring failure to delete C:\\Windows\\System32\\libcrypto-1_1-x64.dll)
-    DEL C:\\Windows\\System32\\libssl-1_1-x64.dll || (Echo Ignoring failure to delete C:\\Windows\\System32\\libssl-1_1-x64.dll)
-)
-
-:: Make paths like C:\\hostedtoolcache\\windows\\Ruby\\2.5.7\\x64\\bin garbage
-set "PATH=%PATH:ostedtoolcache=%"
-
-mkdir "%CONDA%\\etc\\conda\\activate.d"
-
-echo set "CONDA_BLD_PATH=%CONDA_BLD_PATH%"         > "%CONDA%\\etc\\conda\\activate.d\\conda-forge-ci-setup-activate.bat"
-echo set "CPU_COUNT=%CPU_COUNT%"                  >> "%CONDA%\\etc\\conda\\activate.d\\conda-forge-ci-setup-activate.bat"
-echo set "PYTHONUNBUFFERED=%PYTHONUNBUFFERED%"    >> "%CONDA%\\etc\\conda\\activate.d\\conda-forge-ci-setup-activate.bat"
-echo set "PATH=%PATH%"                            >> "%CONDA%\\etc\\conda\\activate.d\\conda-forge-ci-setup-activate.bat"
-
-conda info
-conda config --show-sources
-conda list --show-channel-urls
-""")
-
-azure_win_script = literal_unicode("""\
-setlocal EnableExtensions EnableDelayedExpansion
-call %CONDA%\\condabin\\conda_hook.bat
-call %CONDA%\\condabin\\conda.bat activate base
-
-set "FEEDSTOCK_ROOT=%cd%"
-
-call conda config --append channels defaults
-call conda config --add channels conda-forge
-call conda config --add channels robostack
-call conda config --set channel_priority strict
-
-:: conda remove --force m2-git
-
-C:\\Miniconda\\python.exe -m pip install git+https://github.com/mamba-org/boa.git@master
-if errorlevel 1 exit 1
-
-for %%X in (%CURRENT_RECIPES%) do (
-    echo "BUILDING RECIPE %%X"
-    cd %FEEDSTOCK_ROOT%\\recipes\\%%X\\
-    copy %FEEDSTOCK_ROOT%\\conda_build_config.yaml .\\conda_build_config.yaml
-    boa build .
-    if errorlevel 1 exit 1
-)
-
-anaconda -t %ANACONDA_API_TOKEN% upload "C:\\bld\\win-64\\*.tar.bz2" --force
-if errorlevel 1 exit 1
-""")
+import requests
 
 parsed_args = None
 
 
 def parse_command_line(argv):
     parser = argparse.ArgumentParser(
-        description="Conda recipe Azure pipeline generator for ROS packages"
+        description="Conda recipe Dot graphic generator for ROS packages"
     )
 
     default_dir = "./recipes"
@@ -172,52 +45,6 @@ def parse_command_line(argv):
     global parsed_args
     parsed_args = arguments
     return arguments
-
-
-def normalize_name(s):
-    s = s.replace("-", "_")
-    return re.sub("[^a-zA-Z0-9_]+", "", s)
-
-
-def batch_stages(stages, max_batch_size=5):
-    with open("vinca.yaml", "r") as vinca_yaml:
-        vinca_conf = yaml.safe_load(vinca_yaml)
-
-    # this reduces the number of individual builds to try to save some time
-    stage_lengths = [len(s) for s in stages]
-    merged_stages = []
-    curr_stage = []
-    build_individually = vinca_conf.get("build_in_own_azure_stage", [])
-
-    def chunks(lst, n):
-        """Yield successive n-sized chunks from lst."""
-        for i in range(0, len(lst), n):
-            yield lst[i:i + n]
-    i = 0
-    while i < len(stages):
-        for build_individually_pkg in build_individually:
-            if build_individually_pkg in stages[i]:
-                merged_stages.append([[build_individually_pkg]])
-                stages[i].remove(build_individually_pkg)
-
-        if stage_lengths[i] < max_batch_size and len(curr_stage) + stage_lengths[i] < max_batch_size:
-            # merge with previous stage
-            curr_stage += stages[i]
-        else:
-            if len(curr_stage):
-                merged_stages.append([curr_stage])
-                curr_stage = []
-            if stage_lengths[i] < max_batch_size:
-                curr_stage += stages[i]
-            else:
-                # split this stage into multiple
-                merged_stages.append(list(chunks(stages[i], max_batch_size)))
-        i += 1
-    if len(curr_stage):
-        merged_stages.append([curr_stage])
-    return merged_stages
-
-import requests
 
 def get_skip_existing(vinca_conf, platform):
     fn = vinca_conf.get("skip_existing")
