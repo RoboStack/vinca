2e16e89f603e15b46fef8fa3d0c969cae6fa2af6
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-06-21 18:10:32 +0200

    allow triggering new versions from YAML

--- vinca/main.py
+++ vinca/main.py
@@ -882,7 +882,7 @@ def main():
     if arguments.trigger_new_versions:
         vinca_conf["trigger_new_versions"] = True
     else:
-        vinca_conf["trigger_new_versions"] = False
+        vinca_conf["trigger_new_versions"] = vinca_conf.get("trigger_new_versions", False)
 
     if arguments.package:
         pkg_files = glob.glob(arguments.package)
