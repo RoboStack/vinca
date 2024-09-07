0c3783a3ba0d717bf3fb14640c42041ef8a95930
Jayden Grubb <jaydengrubb1@gmail.com>
2024-09-07 12:02:26 +1000

    Use Vinca Distro instead of disros

--- vinca/snapshot.py
+++ vinca/snapshot.py
@@ -1,10 +1,6 @@
 import argparse
-import os
 import yaml
-
-from rosdistro import get_index, get_index_url, get_cached_distribution
-from rosdistro.dependency_walker import DependencyWalker
-from rosdistro.manifest_provider import get_release_tag
+from .distro import Distro
 
 
 def main():
@@ -30,34 +26,6 @@ def main():
         required=False,
     )
     parser.add_argument(
-        "-t",
-        "--type",
-        type=str,
-        dest="types",
-        nargs="+",
-        default=[
-            "buildtool",
-            "buildtool_export",
-            "build",
-            "build_export",
-            "run",
-            "test",
-            "exec",
-        ],
-        help="Dependency type to get (default: all)",
-        required=False,
-    )
-    parser.add_argument(
-        "-s",
-        "--system",
-        type=str,
-        dest="system",
-        choices=["linux", "osx", "win64"],
-        default="linux",
-        help="System to get dependencies for (default: linux)",
-        required=False,
-    )
-    parser.add_argument(
         "-o",
         "--output",
         type=str,
@@ -76,25 +44,8 @@ def main():
     )
     args = parser.parse_args()
 
-    index = get_index(get_index_url())
-    distro = get_cached_distribution(index, args.distro)
-
-    python_version = index.distributions[args.distro]["python_version"]
-    os.environ["ROS_PYTHON_VERSION"] = "{0}".format(python_version)
-    os.environ["ROS_DISTRO"] = "{0}".format(args.distro)
-    if "ROS_ROOT" in os.environ:
-        os.environ.pop("ROS_ROOT")
-    if "ROS_PACKAGE_PATH" in os.environ:
-        os.environ.pop("ROS_PACKAGE_PATH")
-
-    walker = DependencyWalker(distro, os.environ)
-
-    deps = walker.get_recursive_depends(
-        pkg_name=args.package,
-        depend_types=args.types,
-        ros_packages_only=True,
-        ignore_pkgs=None,
-    )
+    distro = Distro(args.distro)
+    deps = distro.get_depends(args.package)
     deps.add(args.package)
 
     if not args.quiet:
@@ -104,14 +55,12 @@ def main():
     output = {}
 
     for dep in deps:
-        pkg = distro.release_packages[dep]
-        repo = distro.repositories[pkg.repository_name].release_repository
-        tag = get_release_tag(repo, dep)
-        version = repo.version.split("-")[0]
+        url, tag = distro.get_released_repo(dep)
+        version = distro.get_version(dep)
+        output[dep] = {"url": url, "version": version, "tag": tag}
 
         if not args.quiet:
             print("{0:{2}} {1}".format(dep, version, max_len + 2))
-        output[dep] = {"url": repo.url, "version": version, "tag": tag}
 
     with open(args.output, "w") as f:
         yaml.dump(output, f)
