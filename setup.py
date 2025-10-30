e4974f5326d48c885c99cad6bb95fba38766d805
Tobias Fischer <info@tobiasfischer.info>
2025-10-30 12:18:42 +1000

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-args = {
-    "include_package_data": True,
-    "packages": find_packages(),
-    "package_data": {"vinca": ["templates/*", "azure_templates/*"]},
-}
-
-setup(**args)
